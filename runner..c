56e38e899833c5f2ba4fe9590b8c71532e54a1b0
Loi-Luiz Fernando <loi7777@hotmail.com>
2024-11-18 00:27:43 -0400

    Elaborado oque iremos fazer no trabalho

---
+++ runner..c
@@ -0,0 +1,44 @@
+// Luiz Fernando, Bruno Dalponte
+
+
+/*
+    This is the main file that runs our tests.
+
+    Our tests include
+        UDP testing of 1 mB transference
+        TCP testing of 1 mB transference
+    
+    These will be tested using
+        Short messages (minimal).
+        Longest possible messages.
+        Window behaviour (For TCP)
+        no Window behaviour (For TCP)
+
+    We will monitor the following results
+        Package lost (re-sent message)
+        Overall transference time
+        Network path consistency (in porcentage)
+*/
+
+
+int main(int argc, char argv[]) {
+    // Prepare workspace
+
+
+
+    // Run UDP test
+
+
+
+    // Run TCP test, no window
+
+
+
+    // Run TCP test, window
+
+
+
+    // Finish workspace
+
+
+}
